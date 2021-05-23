f695f1df8bc5c76237f5d9e425374a449b776259
gemumeka <justkrut514@gmail.com>
2021-05-23 13:42:52 +0300

    fix: remove test function from util

--- src/util.js
+++ src/util.js
@@ -4,9 +4,4 @@ function readFileAsText(path) {
     return fs.readFileSync(path, "utf-8");
 }
 
-function sum(a, b) {
-    return a + b;
-}
-
-exports.readFileAsText = readFileAsText;
-exports.sum = sum;
+module.exports.readFileAsText = readFileAsText;
