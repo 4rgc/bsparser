d5ccd62888001ef6d727b1e8394ad5cd061479ca
Andrew <bohdan.andrii@gmail.com>
2021-05-28 17:15:00 +0300

    add: Errors module with custom error types

---
+++ src/Errors.ts
@@ -0,0 +1,25 @@
+import { RawTransaction } from './Transactions';
+import { Pattern } from './types';
+
+export class MultipleMatchingPatternsFoundError extends Error {
+	constructor(
+		rawTransaction: RawTransaction,
+		matchingPatterns: (Pattern | void)[]
+	) {
+		super();
+		this.name = 'MultipleMatchingPatternsFoundError';
+		this.message = `error: multiple matching patterns found\n\
+                        transaction:\n\
+                        ${JSON.stringify(rawTransaction)}\n\
+                        patterns:\n\
+                        ${JSON.stringify(matchingPatterns)}`;
+	}
+}
+
+export class InvalidNumberChoiceError extends Error {
+	constructor(from: number, to: number) {
+		super();
+		this.name = 'InvalidChoiceError';
+		this.message = `invalid choice, expected ${from}-${to}`;
+	}
+}
