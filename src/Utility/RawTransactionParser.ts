3fa953daf915d1726db2583d9ea43846ba8cddb6
gemumeka <justkrut514@gmail.com>
2021-06-02 12:27:56 +0300

    refactor: separate multiline and row parsing logic

--- src/Utility/RawTransactionParser.ts
+++ src/Utility/RawTransactionParser.ts
@@ -1,10 +1,31 @@
 import { RawTransaction } from '../Transactions';
 import { ArgumentError } from './Errors';
 
-export default class RawTransactionParser {
-	static fromCSV(data: string): RawTransaction {
-		const props = data.split(',');
+const FormattersEnum = Object.freeze({
+	csv: (row: string): string[] => {
+		return row.split(',');
+	},
+	tsv: (row: string): string[] => {
+		return row.split('\t');
+	},
+});
 
+export default class RawTransactionParser {
+	static fromMultiline(
+		data: string,
+		format: string & keyof typeof FormattersEnum
+	): RawTransaction[] {
+		const rows = data.split('\n');
+		const ret = [];
+		for (const row of rows) {
+			const props = FormattersEnum[format](row);
+			const transaction = RawTransactionParser.buildObject(props);
+			ret.push(transaction);
+		}
+		return ret;
+	}
+
+	private static buildObject(props: string[]) {
 		if (
 			/^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/.test(
 				props[0]
@@ -13,10 +34,14 @@ export default class RawTransactionParser {
 		)
 			throw new ArgumentError('must be a valid date following MM/DD/YY');
 
-		return {
+		if (isNaN(Number.parseFloat(props[2])))
+			throw new ArgumentError('must be a valid number');
+
+		const transaction = {
 			date: props[0],
 			desc: props[1],
-			amount: Number.parseInt(props[2]),
+			amount: Number.parseFloat(props[2]),
 		};
+		return transaction;
 	}
 }
--- test/rawTransactionParser.test.ts
+++ test/rawTransactionParser.test.ts
@@ -3,24 +3,30 @@ import RawTransactionParser from '../src/Utility/RawTransactionParser';
 import { testRawTransaction } from './testutils';
 
 describe('RawTransactionParser', () => {
-	describe('fromCSV()', () => {
+	describe('fromMultiline()', () => {
 		const testRow = `4/1/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`,
 			testThrowRow = `40/1/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`;
-		test('should return a RawTransaction', () => {
+
+		test('should return a RawTransaction array', () => {
+			const result = RawTransactionParser.fromMultiline(testRow, 'csv');
+			expect(result).toBeArray();
 			for (const k of Object.keys(testRawTransaction))
-				expect(RawTransactionParser.fromCSV(testRow)).toHaveProperty(k);
+				expect(result[0]).toHaveProperty(k);
 		});
+
 		test('should return an object with a proper date prop', () => {
-			const date = RawTransactionParser.fromCSV(testRow).date;
+			const date = RawTransactionParser.fromMultiline(testRow, 'csv')[0]
+				.date;
 			expect(date).toMatch(
 				/^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
 			);
 			expect(new Date(date)).toBeValidDate();
 		});
+
 		test('should throw on invalid date', () => {
-			expect(() => RawTransactionParser.fromCSV(testThrowRow)).toThrow(
-				ArgumentError
-			);
+			expect(() =>
+				RawTransactionParser.fromMultiline(testThrowRow, 'csv')
+			).toThrow(ArgumentError);
 		});
 	});
 });
