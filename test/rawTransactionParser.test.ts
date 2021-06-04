60a8b1895a1a58f5f406d454e14d11c91c7b816a
gemumeka <justkrut514@gmail.com>
2021-06-05 00:05:30 +0300

    test: modify dates and check for invalid month in RTParser

--- test/rawTransactionParser.test.ts
+++ test/rawTransactionParser.test.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { ArgumentError } from '../src/Utility/Errors';
 import RawTransactionParser from '../src/Utility/RawTransactionParser';
 import { testRawTransaction } from './testutils';
@@ -5,9 +6,11 @@ import { testRawTransaction } from './testutils';
 describe('RawTransactionParser', () => {
 	describe('fromMultiline()', () => {
 		describe('using csv', () => {
-			const testRow = `4/1/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`,
-				testDoubleQuoteRow = `4/1/2021,"DOMINOS PIZZA  10754     ""BROOKLYN""   NY ",-24.98`,
-				testThrowRow = `40/1/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`;
+			const testRow = `12/24/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`,
+				testDoubleQuoteRow = `12/24/2021,"DOMINOS PIZZA  10754     ""BROOKLYN""   NY ",-24.98`,
+				testRowWithEmpty = `12/24/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98\n\n\n`,
+				testThrowRow1 = `13/12/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`,
+				testThrowRow2 = `12/40/2021,"DOMINOS PIZZA  10754     BROOKLYN   NY ",-24.98`;
 
 			test('should return a RawTransaction array', () => {
 				const result = RawTransactionParser.fromMultiline(
@@ -27,7 +30,7 @@ describe('RawTransactionParser', () => {
 				expect(date).toMatch(
 					/^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
 				);
-				expect(new Date(date)).toBeValidDate();
+				expect(moment(date, 'MM/DD/YYYY').toDate()).toBeValidDate();
 			});
 
 			test('should remove wrapping double quotes from desc prop', () => {
@@ -59,14 +62,19 @@ describe('RawTransactionParser', () => {
 
 			test('should throw on invalid date', () => {
 				expect(() =>
-					RawTransactionParser.fromMultiline(testThrowRow, 'csv')
+					RawTransactionParser.fromMultiline(testThrowRow1, 'csv')
+				).toThrow(ArgumentError);
+
+				expect(() =>
+					RawTransactionParser.fromMultiline(testThrowRow2, 'csv')
 				).toThrow(ArgumentError);
 			});
 		});
 		describe('using tsv', () => {
-			const testRow = `4/1/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98`,
-				testRow2 = `4/1/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98\n\n\n`,
-				testThrowRow = `40/1/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98`;
+			const testRow = `12/24/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98`,
+				testRowWithEmpty = `12/24/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98\n\n\n`,
+				testThrowRow1 = `13/1/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98`,
+				testThrowRow2 = `12/40/2021\tDOMINOS PIZZA  10754     BROOKLYN   NY \t-24.98`;
 
 			test('should return a RawTransaction array', () => {
 				const result = RawTransactionParser.fromMultiline(
@@ -86,12 +94,12 @@ describe('RawTransactionParser', () => {
 				expect(date).toMatch(
 					/^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
 				);
-				expect(new Date(date)).toBeValidDate();
+				expect(moment(date, 'MM/DD/YYYY').toDate()).toBeValidDate();
 			});
 
 			test('should return one object', () => {
 				const result = RawTransactionParser.fromMultiline(
-					testRow2,
+					testRowWithEmpty,
 					'tsv'
 				);
 				expect(result).toBeArray();
@@ -100,7 +108,11 @@ describe('RawTransactionParser', () => {
 
 			test('should throw on invalid date', () => {
 				expect(() =>
-					RawTransactionParser.fromMultiline(testThrowRow, 'tsv')
+					RawTransactionParser.fromMultiline(testThrowRow1, 'tsv')
+				).toThrow(ArgumentError);
+
+				expect(() =>
+					RawTransactionParser.fromMultiline(testThrowRow2, 'tsv')
 				).toThrow(ArgumentError);
 			});
 		});
