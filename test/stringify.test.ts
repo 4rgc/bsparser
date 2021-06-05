7841b671397afffd2758bfa6e960762e678791f4
gemumeka <justkrut514@gmail.com>
2021-06-05 10:57:24 +0300

    test: Stringify undefined MT fields

--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -1,19 +1,39 @@
 import { Stringify } from '../src/Utility/Stringify';
 import { MeaningfulTransaction } from '../src/Transactions';
-import { testMeaningfulTransaction, tsvTransactionOrder } from './testutils';
+import {
+	testMeaningfulTransaction,
+	MeaningfulTransactionOrder,
+} from './testutils';
 
 describe('Stringify', () => {
-	test('MeaningfulTransaction()', () => {
-		const mt = testMeaningfulTransaction;
-		const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
-		const splitTsvMt = tsvMt.split('\t');
+	describe('MeaningfulTransaction()', () => {
+		let mt: MeaningfulTransaction;
 
-		for (let i = 0; i < tsvTransactionOrder.length; i++) {
-			const orderElem = tsvTransactionOrder[
-				i
-			] as keyof MeaningfulTransaction;
-			const prop = mt[orderElem];
-			expect(splitTsvMt[i]).toBe(prop?.toString());
-		}
+		beforeEach(() => {
+			mt = Object.assign({}, testMeaningfulTransaction);
+		});
+
+		test('should return a proper string', () => {
+			const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
+			const splitTsvMt = tsvMt.split('\t');
+
+			for (let i = 0; i < MeaningfulTransactionOrder.length; i++) {
+				const orderElem = MeaningfulTransactionOrder[
+					i
+				] as keyof MeaningfulTransaction;
+				const prop = mt[orderElem];
+				expect(splitTsvMt[i]).toBe(prop?.toString());
+			}
+		});
+
+		test('should replace undefined fields with ""', () => {
+			mt.Details = undefined;
+			const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
+			const splitTsvMt = tsvMt.split('\t');
+
+			const undefIndex = MeaningfulTransactionOrder.indexOf('Details');
+			expect(mt[MeaningfulTransactionOrder[undefIndex]]).toBe(undefined);
+			expect(splitTsvMt[undefIndex]).toBe('');
+		});
 	});
 });
