e3918859f0f871b973a2df4b024dff0ca2d954d4
gemumeka <justkrut514@gmail.com>
2021-06-07 13:33:05 +0300

    test: cover Stringify csv formatter logic

--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -5,6 +5,7 @@ import {
 	MeaningfulTransactionOrder,
 } from './testutils';
 import { FormatterError } from '../src/Utility/Errors';
+import parse from 'csv-parse/lib/sync';
 
 describe('Stringify', () => {
 	describe('MeaningfulTransaction()', () => {
@@ -14,34 +15,98 @@ describe('Stringify', () => {
 			mt = Object.assign({}, testMeaningfulTransaction);
 		});
 
-		test('should return a proper string', () => {
-			const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
-			const splitTsvMt = tsvMt.split('\t');
+		describe('csv', () => {
+			test('should return a proper string', () => {
+				const csvMt = Stringify.MeaningfulTransaction('csv', mt);
+				const splitCsvMt = parse(csvMt)[0];
 
-			for (let i = 0; i < MeaningfulTransactionOrder.length; i++) {
-				const orderElem = MeaningfulTransactionOrder[
-					i
-				] as keyof MeaningfulTransaction;
-				const prop = mt[orderElem];
-				expect(splitTsvMt[i]).toBe(prop?.toString());
-			}
+				for (let i = 0; i < MeaningfulTransactionOrder.length; i++) {
+					const orderElem = MeaningfulTransactionOrder[
+						i
+					] as keyof MeaningfulTransaction;
+					const prop = mt[orderElem];
+					expect(splitCsvMt[i]).toBe(prop?.toString());
+				}
+			});
+
+			test('should replace undefined fields with ""', () => {
+				mt.Details = undefined;
+				const csvMt = Stringify.MeaningfulTransaction('csv', mt);
+				const splitCsvMt = parse(csvMt)[0];
+
+				const undefIndex =
+					MeaningfulTransactionOrder.indexOf('Details');
+				expect(mt[MeaningfulTransactionOrder[undefIndex]]).toBe(
+					undefined
+				);
+				expect(splitCsvMt[undefIndex]).toBe('');
+			});
+
+			test('should wrap Contents prop in double quotes', () => {
+				const testContents = 'The BRUH, store';
+				mt.Contents = testContents;
+				const csvMt = Stringify.MeaningfulTransaction('csv', mt);
+				const splitCsvMt = parse(csvMt)[0];
+
+				const contentsIdx =
+					MeaningfulTransactionOrder.indexOf('Contents');
+
+				expect(mt[MeaningfulTransactionOrder[contentsIdx]]).toBe(
+					testContents
+				);
+				console.log(splitCsvMt);
+				expect(splitCsvMt[contentsIdx]).toBe('The BRUH, store');
+			});
+
+			test('should wrap and replace inner double quotes with 2 of those', () => {
+				const testContents = 'The "BRUH" store';
+				mt.Contents = testContents;
+				const csvMt = Stringify.MeaningfulTransaction('csv', mt);
+				const splitCsvMt = parse(csvMt)[0];
+
+				const contentsIdx =
+					MeaningfulTransactionOrder.indexOf('Contents');
+
+				expect(mt[MeaningfulTransactionOrder[contentsIdx]]).toBe(
+					testContents
+				);
+				expect(splitCsvMt[contentsIdx]).toBe('The "BRUH" store');
+			});
 		});
 
-		test('should replace undefined fields with ""', () => {
-			mt.Details = undefined;
-			const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
-			const splitTsvMt = tsvMt.split('\t');
+		describe('tsv', () => {
+			test('should return a proper string', () => {
+				const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
+				const splitTsvMt = tsvMt.split('\t');
 
-			const undefIndex = MeaningfulTransactionOrder.indexOf('Details');
-			expect(mt[MeaningfulTransactionOrder[undefIndex]]).toBe(undefined);
-			expect(splitTsvMt[undefIndex]).toBe('');
-		});
+				for (let i = 0; i < MeaningfulTransactionOrder.length; i++) {
+					const orderElem = MeaningfulTransactionOrder[
+						i
+					] as keyof MeaningfulTransaction;
+					const prop = mt[orderElem];
+					expect(splitTsvMt[i]).toBe(prop?.toString());
+				}
+			});
 
-		test('should throw on field containing \\t', () => {
-			mt['Main Cat.'] += '\t';
-			expect(() => Stringify.MeaningfulTransaction('tsv', mt)).toThrow(
-				FormatterError
-			);
+			test('should replace undefined fields with ""', () => {
+				mt.Details = undefined;
+				const tsvMt = Stringify.MeaningfulTransaction('tsv', mt);
+				const splitTsvMt = tsvMt.split('\t');
+
+				const undefIndex =
+					MeaningfulTransactionOrder.indexOf('Details');
+				expect(mt[MeaningfulTransactionOrder[undefIndex]]).toBe(
+					undefined
+				);
+				expect(splitTsvMt[undefIndex]).toBe('');
+			});
+
+			test('should throw on field containing \\t', () => {
+				mt['Main Cat.'] += '\t';
+				expect(() =>
+					Stringify.MeaningfulTransaction('tsv', mt)
+				).toThrow(FormatterError);
+			});
 		});
 	});
 });
